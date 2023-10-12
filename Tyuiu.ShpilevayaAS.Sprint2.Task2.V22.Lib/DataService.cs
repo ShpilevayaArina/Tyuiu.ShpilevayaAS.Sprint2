b1614f977866c6f079e11e536c72e8183f75a752
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-10-13 03:22:29 +0500

    Task2

---
+++ Tyuiu.ShpilevayaAS.Sprint2.Task2.V22.Lib/DataService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ShpilevayaAS.Sprint2.Task2.V22.Lib
+{
+    public class DataService : tyuiu.cources.programming.interfaces.Sprint2.ISprint2Task2V22
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res = new bool();
+
+            if ((x >= 3) && (x <=13) && (y >=3) && (y <= 14))
+            {
+                if ((x >= 3) && (x <= 5) && (y >= 3) && (y <= 7))
+                {
+                    res = true;
+                }
+                if ((x >= 9) && (x <= 12) && (y >= 3) && (y <= 4))
+                {
+                    res = true;
+                }
+                if ((x >= 6) && (x <= 12) && (y >= 5) && (y <= 7))
+                {
+                    res = true;
+                }
+                if ((x >= 13) && (x <= 13) && (y >= 6) && (y <= 8))
+                {
+                    res = true;
+                }
+                if ((x >= 12) && (x <= 12) && (y >= 8) && (y <= 11))
+                {
+                    res = true;
+                }
+                if ((x >= 9) && (x <= 10) && (y >= 8) && (y <= 9))
+                {
+                    res = true;
+                }
+                if ((x >= 4) && (x <= 6) && (y >= 8) && (y <= 11))
+                {
+                    res = true;
+                }
+                if ((x >= 3) && (x <= 3) && (y >=11) && (y <= 11))
+                {
+                    res = true;
+                }
+                if ((x >= 5) && (x <= 5) && (y >= 12) && (y <= 13))
+                {
+                    res = true;
+                }
+                if ((x >= 4) && (x <= 10) && (y >= 14) && (y <= 14))
+                {
+                    res = true;
+                }
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.ShpilevayaAS.Sprint2.Task2.V22/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShpilevayaAS.Sprint2.Task2.V22.Lib;
+
+namespace Tyuiu.ShpilevayaAS.Sprint2.Task2.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.Title = "Спринт #2 | Выполнила: Шпилевая А. С. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Шпилевая Арина Станиславовна | ИИПб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и   *");
+            Console.WriteLine("* вычисляет,находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+            Console.ReadKey();
+        }
+    }
+}
